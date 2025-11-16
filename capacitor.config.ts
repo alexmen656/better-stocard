98f32421deba6b6dfc41b3f15276f8ad61f40663
Alex <133679349+alexmen656@users.noreply.github.com>
2025-11-16 02:53:33 +0100

    feat: Initial capacitor setup

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.reelmia.bs',
+  appName: 'better-stocard',
+  webDir: 'dist'
+};
+
+export default config;
